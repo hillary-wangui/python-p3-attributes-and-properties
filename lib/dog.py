32371792d97859d5adb4d3dd2f0db2e7eb2c4651
hillary-wangui <hillary.maina@student.moringaschool.com>
2024-01-09 04:28:33 +0300

    assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,46 @@
 #!/usr/bin/env python3
 
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
 
 class Dog:
-    pass
+    APPROVED_BREEDS = [
+        "Mastiff",
+        "Chihuahua",
+        "Corgi",
+        "Shar Pei",
+        "Beagle",
+        "French Bulldog",
+        "Pug",
+        "Pointer"
+    ]
+    def __init__(self, name="Fido", breed="Beagle"):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = name
+
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, value):
+        if value not in self.APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = value
+
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
+    
+guido = Dog("Fido", "Beagle")
+
+guido.name = 1
+guido.breed = "Jack"
+
+print(guido.name)
+print(guido.breed)
--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
+
+class Person:
+    APPROVED_JOBS = [
     "Admin",
     "Customer Service",
     "Human Resources",
@@ -14,6 +16,31 @@ APPROVED_JOBS = [
     "Marketing",
     "Purchasing"
 ]
+    def __init__(self, name="Guido", job="Sales"):
+        self.name = name
+        self.job = job
+    
+    def get_name(self):
+        return self._name
+    def set_name(self, name):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = name.title()
+    
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        if job not in self.APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = job
+    
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
 
-class Person:
-    pass
+guido = Person("Guido", "Sales")
+
+print(guido.name)
+print(guido.job)
